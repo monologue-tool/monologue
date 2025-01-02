8d4a9baa6457e5e9d2753c1f02312fa152820a29
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2025-01-02 13:30:42 +0100

    Timeline cells are now moveable

--- common/layouts/character_edit/layer_timeline.gd
+++ common/layouts/character_edit/layer_timeline.gd
@@ -5,6 +5,9 @@ class_name LayerTimeline extends PanelContainer
 
 var timeline_section: Control
 var timeline_cell := preload("res://common/layouts/character_edit/cell.tscn")
+var placement_indicator := preload("res://common/layouts/character_edit/placement_indicator.tscn")
+
+var current_indicator: Control
 
 
 func _ready() -> void:
@@ -14,8 +17,35 @@ func _ready() -> void:
 func add_cell() -> TimelineCell:
 	var new_cell := timeline_cell.instantiate()
 	hbox.add_child(new_cell)
+	new_cell.connect("button_down", _on_cell_button_down.bind(new_cell))
+	new_cell.connect("button_up", _on_cell_button_up.bind(new_cell))
 	
 	return new_cell
+
+
+func _on_cell_button_down(cell: TimelineCell) -> void:
+	current_indicator = placement_indicator.instantiate()
+	hbox.add_child(current_indicator)
+	hbox.move_child(current_indicator, cell.get_index()+1)
+
+
+func _on_cell_button_up(cell: TimelineCell) -> void:
+	hbox.move_child(cell, current_indicator.get_index())
+	
+	current_indicator.queue_free()
+	current_indicator = null
+
+
+func _process(_delta: float) -> void:
+	if current_indicator == null:
+		return
+	
+	var indicator_dist: float = current_indicator.global_position.x - get_global_mouse_position().x
+	var indicator_index: int = current_indicator.get_index()
+	if indicator_dist > 13:
+		hbox.move_child(current_indicator, indicator_index-1)
+	elif indicator_dist <= -13:
+		hbox.move_child(current_indicator, indicator_index+1)
 
 
 func remove_cell(pos: int) -> void:
--- common/layouts/character_edit/timeline_cell.gd
+++ common/layouts/character_edit/timeline_cell.gd
@@ -1,6 +1,9 @@
 class_name TimelineCell extends PanelContainer
 
 
+signal button_down
+signal button_up
+
 @onready var single_cell_texture := preload("res://ui/assets/icons/cell_single.svg")
 @onready var empty_cell_texture := preload("res://ui/assets/icons/cell_empty.svg")
 @onready var left_cell_texture := preload("res://ui/assets/icons/cell_left.svg")
@@ -12,3 +15,7 @@ class_name TimelineCell extends PanelContainer
 
 func _ready() -> void:
 	texture_rect.texture = empty_cell_texture
+
+
+func _on_button_button_down() -> void: button_down.emit()
+func _on_button_button_up() -> void: button_up.emit()
