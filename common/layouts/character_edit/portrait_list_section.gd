b3c78468e5101d62ab1bf0e43babecc797ba0cc2
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2025-01-13 23:19:23 +0100

    Portrait list support scroll

--- common/layouts/character_edit/portrait_list_section.gd
+++ common/layouts/character_edit/portrait_list_section.gd
@@ -4,11 +4,10 @@ class_name PortraitListSection extends CharacterEditSection
 signal portrait_selected
 
 @onready var character_edit := $"../../../../../.."
-@onready var portrait_vbox := %PortraitVBox
 @onready var portrait_settings_section := %PortraitSettingsSection
 @onready var timeline_section := %TimelineSection
 
-@onready var field_vbox := $FieldVBox
+@onready var field_vbox := $ScrollContainer/FieldVBox
 
 var portraits := Property.new(MonologueGraphNode.LIST, {}, [])
 
@@ -28,13 +27,6 @@ func trickle() -> void:
 	var child_sections = [portrait_settings_section, timeline_section]
 	for section in child_sections:
 		section.portrait_index = selected
-
-
-#func _on_add_button_pressed() -> void:
-	#var new_portrait = add_option()
-	#portraits[new_portrait.id] = {
-		#"Name": new_portrait.line_edit.text
-	#}
 
 
 func add_portrait(option_dict: Dictionary = {}) -> AbstractPortraitOption:
@@ -71,70 +63,41 @@ func load_portraits(new_portrait_list: Array) -> void:
 	portraits.value = new_portrait_list
 
 
-func clear() -> void:
-	for portrait in portrait_vbox.get_children():
-		portrait.queue_free()
-
-
 func get_default_portrait() -> String:
-	for portrait in portrait_vbox.get_children():
-		if portrait.is_default:
-			return portrait.id
+	#for portrait in portrait_vbox.get_children():
+		#if portrait.is_default:
+			#return portrait.id
 	return ""
 
 
 func _on_portrait_option_pressed(portrait_option: PortraitOption) -> void:
-	save_current_portrait()
-	
 	selected = portrait_option.get_index()
 	
-	for portrait in portrait_vbox.get_children():
-		if portrait is not PortraitOption:
-			continue
-		
-		if portrait == portrait_option:
-			portrait.set_active()
-			trickle()
-			portrait_settings_section._from_dict(portraits.value)
-			continue
-		
-		portrait.release_active()
-	portrait_selected.emit()
-
-
-func save_current_portrait() -> void:
-	if selected >= 0:
-		var portrait: PortraitOption = portrait_vbox.get_child(selected)
-		portraits[portrait.id] = portrait_settings_section._to_dict()
-		portraits[portrait.id]["Name"] = portrait.line_edit.text
+	#for portrait in portrait_vbox.get_children():
+		#if portrait is not PortraitOption:
+			#continue
+		#
+		#if portrait == portrait_option:
+			#portrait.set_active()
+			#trickle()
+			#portrait_settings_section._from_dict(portraits.value)
+			#continue
+		#
+		#portrait.release_active()
+	#portrait_selected.emit()
 
 
 func _on_portrait_option_set_to_default(portrait_option: PortraitOption) -> void:
-	for portrait in portrait_vbox.get_children():
-		if portrait is not PortraitOption:
-			continue
-		
-		if portrait == portrait_option:
-			continue
-		
-		portrait.release_default()
+	#for portrait in portrait_vbox.get_children():
+		#if portrait is not PortraitOption:
+			#continue
+		#
+		#if portrait == portrait_option:
+			#continue
+		#
+		#portrait.release_default()
+	pass
 
 
 func _to_dict() -> Dictionary:
-	save_current_portrait()
 	return portraits.value
-
-
-func _from_dict(dict: Dictionary) -> void:
-	#selected = -1
-	#clear()
-	#var data: Dictionary = dict.get("Portraits", {})
-	#portraits.value = data
-	#
-	#for portrait_id in data.keys():
-		#var portrait_data = data[portrait_id]
-		#var portrait_node := add_option(portrait_data["Name"])
-		#portrait_node.id = portrait_id
-		#if portrait_id == dict.get("DefaultPortrait"):
-			#portrait_node.set_default()
-	super._from_dict(dict)
