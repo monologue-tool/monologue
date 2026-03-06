0bcbc822c24d746895c76b589c39fd7e50b13ba3
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2026-03-07 00:59:06 +0100

    Update id_gen.gd

--- autoloads/id_gen.gd
+++ autoloads/id_gen.gd
@@ -3,18 +3,10 @@ class_name IDGen extends Node
 const CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 
-static func generate(length: int = 10, avoid_ids: Array = []) -> String:
-	for _i in range(100):
-		var id = ""
-		var random = RandomNumberGenerator.new()
-		random.randomize()
+static func generate(length: int = 10) -> String:
+	var id = ""
+	for i in range(length):
+		var index = randi() % CHARS.length()
+		id += CHARS[index]
 
-		for i in range(length):
-			var index = random.randi_range(0, CHARS.length() - 1)
-			id += CHARS[index]
-
-		if id not in avoid_ids:
-			return id
-
-	push_error("The program failed to generate a random number.")
-	return ""
+	return id
